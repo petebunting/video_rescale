dc9b8b6de5140dc057319215a2b5488d44ee1154
Pete Bunting <petebunting@mac.com>
2020-04-11 15:54:10 +0100

    Update.

--- rescale_videos.py
+++ rescale_videos.py
@@ -32,7 +32,7 @@ def get_video_params(video_file):
 def get_video_params(video_file):
     cmd = "ffprobe -v quiet -print_format json -show_streams"
     args = shlex.split(cmd)
-    args.append(pathToInputVideo)
+    args.append(video_file)
     # run the ffprobe process, decode stdout into utf-8 & convert to JSON
     ffprobeOutput = subprocess.check_output(args).decode('utf-8')
     ffprobeOutput = json.loads(ffprobeOutput)
